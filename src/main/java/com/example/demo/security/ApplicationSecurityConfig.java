ea8175d323838d14ff9a13fe79471eaf64dbe9c0
Genuslee Mapedze <genuslee.developer@gmail.com>
2023-01-16 14:22:28 +0200

    feat: passwordEncorder

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,27 +1,34 @@
 package com.example.demo.security;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
+private final PasswordEncoder passwordEncoder;
+
+@Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-//                .authorizeHttpRequests()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
+
 
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest().authenticated()
@@ -31,10 +38,22 @@ public class ApplicationSecurityConfig {
 
         return http.build();
 
+
     }
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring().anyRequest().requestMatchers("/", "/index");
-//    }
+//    @Override
+    @Bean
+    protected UserDetailsService userDetailsService(){
+       UserDetails genusUser = User.builder()
+                .username("genuslee")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                 .build();
+
+        return  new InMemoryUserDetailsManager(
+                genusUser
+        );
+    }
+
+
 
 }
